fe290baaa6bd1f2b1210ec467e1a72aa8f624df9
Laura <joneslae21@gmail.com>
2021-05-28 01:01:03 -0400

    updated dbcon and beds.js

--- dbcon.js
+++ dbcon.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_jonesl7',
-  password        : '****',
+  password        : '2361',
   database        : 'cs340_jonesl7'
 });
 module.exports.pool = pool;
