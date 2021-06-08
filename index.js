202947f8bed7a6aa72ae80c16c5d5877c8a82092
jonesle21 <67295425+jonesle21@users.noreply.github.com>
2021-06-08 00:19:48 -0400

    updates

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const deleteReservations = `DELETE FROM Reservations WHERE reservationID = ? AND
 
 //Customers
 const selectCustomers =  `SELECT customerID, email, firstName, lastName, phone, student, genderRoom FROM Customers;`
-const insertCustomers = `INSERT INTO Customers (email, firstName, lastName, phone, student, genderRoom) VALUES(?,?,?,?,?,?);`;
+const insertCustomers = `INSERT INTO Customers (email, firstName, lastName, phone, student, genderRoom) VALUES(?);`;
 const deleteCustomers = `DELETE FROM Customers WHERE customerID = ? AND firstName = ? AND lastName = ?;`;
 
 
