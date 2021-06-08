0320b46361a9e2a5d75cbded1a05461b2a0b4ef3
anthony-robinson <acrobinson794@gmail.com>
2021-06-07 17:16:07 -0700

    added frontend code for customers.js and customers.html ~notyet fully functional but will test soon. Adding here for save purposes.

--- public/customers.js
+++ public/customers.js
@@ -1,4 +1,7 @@
 
+const baseURL = '/customers'; // for added code below
+let form = document.getElementById('customersForm'); //for post request
+
 module.exports = function(){
     var express = require('express');
     var router = express.Router();
@@ -30,3 +33,118 @@ module.exports = function(){
         }
     });
 }
+
+
+/** ANTHONY's ADDED CODE */
+async function fetchData(){
+    const res = await fetch(baseURL);
+    const data = await res.json(); //returns a promise resolved to a JSON object
+    //waits until request completes
+    //return the response/ do something with response data.
+    return res;
+}
+
+//For populating new table
+function createTdElem(input, row){
+    let elem = document.createElement('td');
+    elem.textContent = input;
+    row.appendChild(elem);
+}
+
+//For deleting an entry in the table
+function deleteEntry(id){
+    let table = document.getElementById("customerTable");
+    let length = document.getElementById("customerTable").rows.length;
+    key = "delete" + id;
+
+    
+    for(let j=1; j < length; j++){
+        let row = table.rows[j];
+        let data = row.getElementsByTagName("td");
+        let deleteEntry = data[data.length - 1];
+        if(deleteEntry.children[1].id === key){
+            document.getElementById("customerTable").deleteRow(j);
+            length--;
+            j--;
+        }
+    }
+    let req = new XMLHttpRequest();
+
+    req.open("DELETE", "/delete", true);
+    req.send("/delete" + id);
+    
+}
+
+//implement Submit functionality
+function bindSubmitButton(){
+    form.addEventListener('submit', function(event){
+        let req = new XMLHttpRequest();
+        let payload = {
+            email: null,
+            firstName: null,
+            lastName: null,
+            phone: null,
+            student: 0,
+            genderRoom: null
+        };
+        payload.email = document.getElementById('email').value;
+        payload.firstName = document.getElementById('firstName').value;
+        payload.lastName = document.getElementById('lastName').value;
+        payload.phone = document.getElementById('phone').value;
+        payload.student = document.getElementById('student').value;
+        payload.genderRoom = document.getElementById('genderRoom').value;
+
+        //open post request
+        req.open('POST', baseURL, true);
+        req.setRequestHeader('Content-Type','application/json');
+        req.addEventListener('load', function(){
+            if(req.status >= 200 && req.status <= 400){
+                //add to table on load
+                let response = JSON.parse(req.responseText);
+                let insertId = response.insertId;//for deletion
+                let table = document.getElementById("customerTable");
+                let newRow = table.insertRow(-1);
+
+                createTdElem(payload.email, newRow);
+                createTdElem(payload.firstName, newRow);
+                createTdElem(payload.lastName, newRow);
+                createTdElem(payload.phone, newRow);
+                createTdElem(payload.student, newRow);
+                createTdElem(payload.genderRoom, newRow);
+
+                //Buttons 
+
+                //edit button
+                let editData = document.createElement('td');
+        
+                let editBtn = document.createElement('input');
+                editBtn.type="submit";
+                editBtn.value = "Edit";
+                editData.appendChild(editBtn);
+                newRow.appendChild(editData);
+                
+                //Delete Button
+                let deleteData = document.createElement('td');
+                let deleteBtn = document.createElement('input');
+                deleteBtn.type = "button";
+                deleteBtn.value = "Delete";
+                deleteBtn.onclick = function(){deleteEntry(insertId);};
+
+                let hidden = document.createElement('input');
+                hidden.type = "hidden";
+                hidden.id = "delete" + insertId;
+                
+                deleteData.appendChild(deleteBtn);
+                deleteData.appendChild(hidden);
+                newRow.appendChild(deleteData);
+            }
+        })
+        req.send(JSON.stringify(input));
+
+        event.preventDefault();
+    })
+}
+
+document.addEventListener('DOMContentLoaded', bindSubmitButton);
+
+
