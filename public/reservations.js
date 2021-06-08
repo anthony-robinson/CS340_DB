6d91b80c56774a7caf8220a46dab1713d2bde327
jonesle21 <67295425+jonesle21@users.noreply.github.com>
2021-06-08 02:36:10 -0400

    update attributes

--- public/reservations.js
+++ public/reservations.js
@@ -1,14 +1,15 @@
 const baseURL = '/reservations'; // for added code below
-let form = document.getElementById('reservationsForm'); //for post request
+let form = document.getElementById('resForm'); //for post reques
+
 
 /** ANTHONY's ADDED CODE */
 async function fetchData(){
-    const res = await fetch(baseURL);
+    const res = await fetch("/reservations");
     const data = await res.json(); //returns a promise resolved to a JSON object
-    //waits until request completes
     buildTable(data);
+    //waits until request completes
     //return the response/ do something with response data.
-    //return res;
+    return res;
 }
 
 //For populating new table
@@ -20,8 +21,8 @@ function createTdElem(input, row){
 
 //For deleting an entry in the table
 function deleteEntry(id){
-    let table = document.getElementById("customerTable");
-    let length = document.getElementById("customerTable").rows.length;
+    let table = document.getElementById("resTable");
+    let length = document.getElementById("resTable").rows.length;
     key = "delete" + id;
 
     
@@ -49,30 +50,42 @@ function bindSubmitButton(){
         event.preventDefault();
         let req = new XMLHttpRequest();
         let payload = {
+            customerID: null,
             resDate: null,
             checkInDate: null,
             checkOutDate: null,
             nights: null,
         };
-        payload.resDate = document.getElementById('resDate').value;
-        payload.checkInDate = document.getElementById("checkInDate").value;
-        payload.checkOutDate = document.getElementById("checkOutDate").value;
-        payload.nights = document.getElementById("nights").value;
-        payload.email = document.getElementById('email').value;
-        
+        payload.customerID = document.getElementById('inputCustomer').value;
+        payload.bedID = document.getElementById('inputBed').value;
+        payload.resDate = document.getElementById('inputResDate').value;
+        payload.checkIn = document.getElementById('inputCheckInDate').value;
+        payload.checkOut = document.getElementById('inputCheckOutDate').value;
+        payload.nights = document.getElementById('nights').value;
+
+
+
+
         //open post request
         req.open('POST', baseURL, true);
         req.setRequestHeader('Content-Type','application/json');
         req.addEventListener('load', function(){
             if(req.status >= 200 && req.status <= 400){
-                //create table on successful load
-                console.log("success");
-                fetchData();
-            }else{
-                console.log("failure " + req.statusText);
+                //add to table on load
+                let response = JSON.parse(req.responseText);
+                let insertId = response.insertId;//for deletion
+                let table = document.getElementById("roomsTable");
+                let newRow = table.insertRow(-1);
+                createTdElem(payload.customerID, newRow);
+                createTdElem(payload.bedID, newRow);
+                createTdElem(payload.resDate, newRow);
+                createTdElem(payload.checkInDate, newRow);
+                createTdElem(payload.checkOutDate, newRow);
+                createTdElem(payload.nights, newRow);
             }
         })
         req.send(JSON.stringify(payload));
+
         
     })
 }
@@ -82,57 +95,19 @@ document.addEventListener('DOMContentLoaded', bindSubmitButton);
 fetchData();
 
 
-
-function buildTable(response){
-    let table = document.getElementById("customerTable");
-
-    const properties = ['email', "firstName", "lastName", "phone", "student", "genderRoom"];
+ function buildTable(response){
+    const table = document.getElementById('reservations');
+    table.innerHTML = "";
+    const properties = ['customerID', "bedID", "resDate", "checkInDate", "checkOutDate", "ngihts"];
     let row, cell, deleteBtn, editBtn, form;
-    let createdTableBody;
-    //clear table
-    if(document.querySelector("tbody") !== null){
-        table.removeChild(document.querySelector("tbody"));
-    }
-    createdTableBody = document.createElement("tbody");
     for(i = 0; i<response.length; i++) {
        row = document.createElement("tr");
-       for(j= 0; j < properties.length; j++){
-        cell = document.createElement("td");
-        cell.appendChild(document.createTextNode(response[i][properties[j]]));
-        row.appendChild(cell);
-    };
-       createdTableBody.appendChild(row);
-        
-       let insertId = response.insertId;//for deletion
- 
-        //Buttons 
+       for(j = 0; j<6; j++) {
+          cell = document.createElement("td");
+          cell.textContent = response[i][properties[j]];
+          row.appendChild(cell);
+       }
 
-        //edit button
-        let editData = document.createElement('td');
-
-        let editBtn = document.createElement('input');
-        editBtn.type="submit";
-        editBtn.value = "Edit";
-        editData.appendChild(editBtn);
-        row.appendChild(editData);
-        createdTableBody.appendChild(row);
-        
-        //Delete Button
-        let deleteData = document.createElement('td');
-        let deleteBtn = document.createElement('input');
-        deleteBtn.type = "button";
-        deleteBtn.value = "Delete";
-        deleteBtn.onclick = function(){deleteEntry(insertId);};
-
-        let hidden = document.createElement('input');
-        hidden.type = "hidden";
-        hidden.id = "delete" + i;
-        
-        deleteData.appendChild(deleteBtn);
-        deleteData.appendChild(hidden);
-        row.appendChild(deleteData);
-        createdTableBody.appendChild(row);
-        
+        table.appendChild(row);
     }
-    table.appendChild(createdTableBody);
  }
