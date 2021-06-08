62384443ea10a1c9cc93f009eeefa1925f38a9b4
anthony-robinson <acrobinson794@gmail.com>
2021-06-07 23:11:10 -0700

    changed customers.js

--- public/customers.js
+++ public/customers.js
@@ -2,41 +2,6 @@
 const baseURL = '/customers'; // for added code below
 let form = document.getElementById('customersForm'); //for post request
 
-
-// module.exports = function(){
-//     var express = require('express');
-//     var router = express.Router();
-
-//     function getCustomers(res, mysql, context, complete){
-//         mysql.pool.query("SELECT customerID AS id, email, firstName, lastName, phone, student, genderRoom AS RoomType FROM customers", function(error, results, fields){
-//             if(error){
-//                 res.write(JSON.stringify(error));
-//                 res.end();
-//             }
-//             context.customers  = results;
-//             complete();
-//         });
-//     }
-
-
-//     router.get('/', function(req, res){
-//         var callbackCount = 0;
-//         var context = {};
-//         context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
-//         var mysql = req.app.get('mysql');
-//         getCustomers(res, mysql, context, complete);
-//         function complete(){
-//             callbackCount++;
-//             if(callbackCount >= 1){
-//                 res.render('customers', context);
-//             }
-
-//         }
-//     });
-// }
-
-
-/** ANTHONY's ADDED CODE */
 async function fetchData(){
     const res = await fetch(baseURL);
     const data = await res.json(); //returns a promise resolved to a JSON object
